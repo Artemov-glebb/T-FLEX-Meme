44f368f56ad94698c312fe0e8673584d3afe12c5
WitcherProm <bitavr_sviatik@mail.ru>
2026-01-15 15:27:25 +0300

    Make a basic construction

---
+++ T-FLEX-TRY-NOT-TO-CRY/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace T_FLEX_TRY_NOT_TO_CRY
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
